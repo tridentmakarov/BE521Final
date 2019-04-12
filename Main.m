b03a078abe9a969eef25666311e7bb1ec031d348
tridentmakarov <lukinator130@gmail.com>
2019-04-12 15:15:37 -0400

    Added instructions

--- Main.m
+++ Main.m
@@ -1,2 +1,32 @@
-% Github Commands!
-% 
+%% Github Commands!
+% Create account on github.com
+
+% Open a command prompt (windows)/terminal (Mac)
+% git clone https://github.com/tridentmakarov/BE521Final.git
+
+% cd /BE521Final
+
+%% If you want a new branch
+	% git checkout -b BRANCH_NAME  <<< Only if you want a new branch
+
+%% To push code!
+
+% git add .
+% git commit -m 'COMMIT MESSAGE'
+% git push
+
+%% First time, it'll error
+
+% Follow these instructions
+% https://help.github.com/en/articles/creating-a-personal-access-token-for-the-command-line
+
+% Sign in, 
+% Username: YOUR_USERNAME
+% Password: ACCESS_TOKEN_FROM_INSTRUCTIONS
+
+%% ANNOYING THING MATLAB DOES:
+% It'll do this "processing" thing, I recommend doing this:
+
+% Go to preferences, then Matlab  > General > Source Control
+% disable the automatic source control by selecting "None".
+
