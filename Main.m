00187c4e3d9ac005d31684c93a0ce8ca05ca4969
tridentmakarov <lukinator130@gmail.com>
2019-04-18 15:51:14 -0400

    Changes

--- Main.m
+++ Main.m
@@ -3,3 +3,15 @@ clear all
 close all
 
 %% This will be the main code
+
+% I521_Sub1_Training_ecog
+% I521_Sub1_Training_dg
+% I521_Sub1_Leaderboard_ecog
+
+% I521_Sub2_Training_ecog
+% I521_Sub2_Training_dg
+% I521_Sub2_Leaderboard_ecog
+
+% I521_Sub3_Training_ecog
+% I521_Sub3_Training_dg
+% I521_Sub3_Leaderboard_ecog
