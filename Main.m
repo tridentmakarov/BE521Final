869ac7926757e67fabe93b1e29b9c5d588f403b7
tridentmakarov <lukinator130@gmail.com>
2019-04-18 17:03:32 -0400

    Added files

--- Main.m
+++ Main.m
@@ -1,17 +1,8 @@
 clc
-clear all
+clearvars -except test
 close all
 
 %% This will be the main code
 
-% I521_Sub1_Training_ecog
-% I521_Sub1_Training_dg
-% I521_Sub1_Leaderboard_ecog
-
-% I521_Sub2_Training_ecog
-% I521_Sub2_Training_dg
-% I521_Sub2_Leaderboard_ecog
-
-% I521_Sub3_Training_ecog
-% I521_Sub3_Training_dg
-% I521_Sub3_Leaderboard_ecog
+load('Training_ecog.mat');
+load('Training_dg.mat');
