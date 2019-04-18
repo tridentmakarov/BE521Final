a274a931a1019db001e4b1bd7c0906d7449d366e
tridentmakarov <lukinator130@gmail.com>
2019-04-18 19:11:54 -0400

    Stuff

--- Main.m
+++ Main.m
@@ -4,5 +4,17 @@ close all
 
 %% This will be the main code
 
-load('Training_ecog.mat');
-load('Training_dg.mat');
+load('BE521Final');
+
+save('I521_Sub1_Training_dg.mat', 'I521_Sub1_Training_dg')
+save('I521_Sub1_Training_ecog.mat', 'I521_Sub1_Training_ecog')
+save('I521_Sub1_Leaderboard_ecog.mat', 'I521_Sub1_Leaderboard_ecog')
+
+save('I521_Sub2_Training_dg.mat', 'I521_Sub2_Training_dg')
+save('I521_Sub2_Training_ecog.mat', 'I521_Sub2_Training_ecog')
+save('I521_Sub2_Leaderboard_ecog.mat', 'I521_Sub2_Leaderboard_ecog')
+
+save('I521_Sub3_Training_dg.mat', 'I521_Sub3_Training_dg')
+save('I521_Sub3_Training_ecog.mat', 'I521_Sub3_Training_ecog')
+save('I521_Sub3_Leaderboard_ecog.mat', 'I521_Sub3_Leaderboard_ecog')
+
