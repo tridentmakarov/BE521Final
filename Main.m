04f0af41fb1481dcc6b90e8d1692b973fa3615aa
tridentmakarov <lukinator130@gmail.com>
2019-04-19 14:27:33 -0400

    I figured out how to do the spectrogram

--- Main.m
+++ Main.m
@@ -15,25 +15,18 @@ winDisp = 0.05; %s
 % LLFn = @(x) sum(abs(diff(x)));
 LLFn = @(x) mean(x);
 sampleRate = 1000; %samples/s
+fs = 1000;
+ranges = [5, 15; 20, 25; 75, 115; 125, 160; 160, 175];
 
 for i = 1:size(train_mini_ecog, 2)
 	
 	set = train_mini_ecog(:, i);
 	
-	[LL_data, A, E, ZX, t_int] = MovingWinFeats(set, sampleRate, winLen, winDisp, LLFn);
-% 	LL_data_int = zoInterp(LL_data, 3000);
-% 	LL_data_norm(:, i) = (2 * LL_data_int / (max(LL_data_int) / max(set)))';
-	
-% 	times = linspace(0, length(LL_data_norm) * 1000/1E6, length(LL_data_norm))';
-	
-end
+	[LL_data(i, :), A, E, ZX, t_int] = MovingWinFeats(set, sampleRate, winLen, winDisp, LLFn, ranges);
 
+end
+disp()
 % for i = 1:5
 % 	temp = decimate(train_mini_dg(:, i), 50);
 % 	dg(:, i) = temp(4:length(temp));
 % end
-fs = 1000;
-spectrogram(train_mini_ecog(:, 1),100,50,fs,'yaxis');
-[s, f, t] = spectrogram(train_mini_ecog(:, 1),100,50,fs,'yaxis');
-f = f/pi*180;
-mean(s)
